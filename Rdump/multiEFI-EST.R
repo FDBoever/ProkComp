438051837cc95425b4d347bffdcbf8a046a8e8b2
FDBoever <frederik.deboever@sams.ac.uk>
2018-07-03 18:51:49 +0100

    -

--- Rdump/multiEFI-EST.R
+++ Rdump/multiEFI-EST.R
@@ -13,10 +13,24 @@
 
 ####################
 
+library(seqinr)
+library("seqRFLP")
+library(msa)
+library(ape)
+library(annotate)
+library(phytools)
+library(stringr)
+library(Biostring)
+library(plyr)
+library(bios2mds)
+library(ggtree)
+
+####################
 
 
 
 # Load the csv files (EFI-EST generated)
+#AlkB genes
 maqu_0440 = read.csv('~/Downloads/0440.csv')
 maqu_2843 = read.csv('~/Downloads/MAQU_2843_EFI-EST.csv')
 maqu_0610 = read.csv('~/Downloads/maqu_0610_EFI-EST.csv')
@@ -25,19 +39,33 @@ ABA45_07610 = read.csv('~/Downloads/ABA45_07610_EFI-est.csv')
 ABO_2707 = read.csv('~/Downloads/ABO_2707_EFI-EST.csv')
 ABO_0122 = read.csv('~/Downloads/ABO_0122_EFI_EST.csv')
 
+#ATPsynthase in NKSG1
+MSNKSG1_00556 = read.csv('~/Downloads/MSNKSG1_00556.csv')
+MSNKSG1_14497 = read.csv('~/Downloads/MSNKSG1_14497.csv')
+MSNKSG1_04306 = read.csv('~/Downloads/MSNKSG1_04306.csv')
 
-# select the unique ACCESSION numbers from all the files combined
+
+# ALKB - select the unique ACCESSION numbers from all the files combined
 selectedACCession = unique(c(as.character(maqu_0440 $name),as.character(maqu_2843 $name),as.character(maqu_0610 $name),as.character(MDG893_03170 $name),as.character(ABA45_07610 $name),as.character(ABO_2707 $name),as.character(ABO_0122 $name)))
+
+# ATPASE - select the unique ACCESSION numbers from all the files combined
+selectedACCessionAPTase = unique(c(as.character(MSNKSG1_00556 $name),as.character(MSNKSG1_14497 $name),as.character(MSNKSG1_04306 $name)))
+
+
+
+
 
 #save to file (which is ready to be uplaoded to EFI-EST)
 write(selectedACCession,'data.txt')
+write(selectedACCessionAPTase,'dataATP.txt')
 
 selectCols = c("SUID","BRENDA.ID","CAZY.Name","Class","Description","EC","Family","Gene.Name","Genus","GO.Term","HMP.Body.Site","HMP.Oxygen","IPRO","KEGG.ID","Kingdom","name","NCBI.IDs","Order","Organism","P01.gDNA","selected","Sequence.Length","shared.name","Species","STRING.ID","Superkingdom","Swissprot.Description","Taxonomy.ID","UniProt.Annotation.Status")
 
-uberEST = rbind(maqu_0440[, selectCols], mqu[, selectCols], alkB_DG[, selectCols], maqu_2843[, selectCols])
-head(uberEST)
 
+selectedTaxonIDs = uberEST$Taxonomy.ID
+write(selectedTaxonIDs,'dataTaxonID.txt')
 
+~/Downloads/dbfetch_res1.fasta
 
 # TO DOWNLOAD THE RALTED AA SEQUENCES, (rather creative way of doing this), wget the below
 paste('https://www.uniprot.org/uniprot/?query=',paste(selectedACCession,collapse='+or+'),'&format=fasta ',sep='')
@@ -45,22 +73,101 @@ paste('https://www.uniprot.org/uniprot/?query=',paste(selectedACCession,collapse
 paste('https://www.uniprot.org/uniprot/?query=',paste(selectedACCession[1:10],collapse='+or+'),'&format=fasta ',sep='')
 
 
+####################
+####################
+####################
+
+
+atpD_cytoscape = read.csv('~/Downloads/atpD_180_combined.csv')
+
+selectedACCession = atpD_cytoscape$name
+
+selectedACCessionUNIPROT = paste('UNIPROT:',selectedACCession,sep='')
+seqPerFasta = 200
 nACC = length(selectedACCession)
-nCycles = ceiling(nACC/200)
+nCycles = ceiling(nACC/seqPerFasta)
 
 #UNIPROT is restricted in using 200 query items at the time, so we need some steps to get arount this
 
 for(i in c(1:nCycles)){
 	if(i!=nCycles){
-		start=i*200-200+1
-		end=i*200+1
+		start=i* seqPerFasta-seqPerFasta +1
+		end=i* seqPerFasta +1
 	}else{
-		start= i*200-200+1
+		start= i* seqPerFasta-seqPerFasta +1
 		end= nACC
 	}
-	print(selectedACCession[start:end])
-	download.file(paste('https://www.uniprot.org/uniprot/?query=',paste(selectedACCession[start:end],collapse='+or+'),'&format=fasta',sep=''),destfile=paste('~/part',i,'.fasta',sep=''))
+	#print(selectedACCession[start:end])
+	#download.file(paste('https://www.uniprot.org/uniprot/?query=',paste(selectedACCession[start:end],collapse='+or+'),'&format=fasta',sep=''),destfile=paste('~/part',i,'.fasta',sep=''))
+	
+	#print(selectedACCessionUNIPROT[start:end])
+
+	write(selectedACCessionUNIPROT[start:end],paste(i,'embl_uniprot.txt',sep='_'))
+	print(paste('http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=uniprotkb&id=',URLencode(paste(selectedACCession[start:end],collapse='%2C')),'&format=fasta&style=raw&Retrieve=Retrieve',sep=''))
+	download.file(paste('http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=uniprotkb&id=',URLencode(paste(selectedACCession[start:end],collapse='%2C')),'&format=fasta&style=raw&Retrieve=Retrieve',sep=''), paste('~/',i,'_uniprot_atpd.fasta',sep=''))
+
 }
+
+
+
+##### http://www.ebi.ac.uk/Tools/dbfetch/dbfetch/uniprotkb/
+
+#### 
+paste('http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=uniprotkb&id=',paste(selectedACCession[start:end],collapse='%'),'&format=fasta&style=raw&Retrieve=Retrieve',sep='')
+
+
+
+
+#write(c(paste('UNIPROT:', selectedACCession[start],'\n',sep=''),paste(selectedACCession[(start+1):end],collapse='\nUNPROT:')),'TESTTEST.txt')
+#	print(c('UNIPROT:',paste(selectedACCession[start:end],collapse='\nUNPROT:')))
+
+
+
+
+###########################
+
+sequences <- Biostrings::readAAStringSet('~/1_uniprot_atpd.fasta',format='fasta')
+
+sequence@ranges@NAMES = sub("\\_.*", "", sequence@ranges@NAMES)
+sequence@ranges@NAMES = sub("UNIPROT:", "", sequence@ranges@NAMES)
+
+
+path = "~/Documents/My Data/BRAZIL/Elections/"
+out.file<-""
+file.names <- dir(path, pattern =".txt")
+for(i in 1:length(file.names)){
+  file <- read.table(file.names[i],header=TRUE, sep=";", stringsAsFactors=FALSE)
+  out.file <- rbind(out.file, file)
+}
+ write.table(out.file, file = "cand_Brazil.txt",sep=";", 
+             row.names = FALSE, qmethod = "double",fileEncoding="windows-1252")
+
+
+
+aln <- msa(sequences)
+aln <- msaConvert(aln, type="seqinr::alignment")
+print('align sequences done')
+
+
+# dirty but fast: distance matrix and NJ tree reconstruction, and assesment of monophyly of a genus (CHANGE MARINOBACTER); 
+dist.aln <- dist.alignment(aln, "identity")
+njTree <- njs(dist.aln )
+njTree$edge.length = njTree$edge.length + 0.0000001
+
+p <- ggtree(midpoint.root(njTree),ladderize=TRUE)+geom_treescale() + xlim(0,2)
+p1 <- p + geom_tippoint()+ geom_tiplab(size=2)
+	print(p1)
+
+mnjTree = midpoint.root(njTree)
+mnjTree$tip.label[grep('Marinobacter ',mnjTree$tip.label)]
+
+
+njTree
+
+###########################
+
+
+
 
 
 
